db7caa632b870ef9aa9cc3eaad872bfe8f732583
Ruben (sog laptop) <rubenguldenaar@hotmail.com>
2019-11-05 15:14:23 +0100

    *Ruben Sogeti laptop; extra regel in main toegevoegd; test mergen.

--- src/com/example/helloworldfromscratchv2/HelloWorldFromScratchV2.java
+++ src/com/example/helloworldfromscratchv2/HelloWorldFromScratchV2.java
@@ -2,6 +2,8 @@ package com.example.helloworldfromscratchv2;
 
 public class HelloWorldFromScratchV2 {
     public static void main(String[] args) {
-        System.out.print("Hello World From Scratch V2");
+        System.out.print("Hello World From Scratch V2 \n");
+        System.out.print("Added with sogeti laptop: Ruben \"Ruben (sog laptop)\"\n");
+
     }
 }
