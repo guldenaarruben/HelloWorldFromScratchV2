1b85822f05f206e7e8932667440434471ca70ed9
Ruben (asus) <rubenguldenaar@hotmail.com>
2019-11-05 12:58:27 +0100

    v2 eerste commit to master

---
+++ src/com/example/helloworldfromscratchv2/HelloWorldFromScratchV2.java
@@ -0,0 +1,7 @@
+package com.example.helloworldfromscratchv2;
+
+public class HelloWorldFromScratchV2 {
+    public static void main(String[] args) {
+        System.out.print("Hello World From Scratch V2");
+    }
+}
